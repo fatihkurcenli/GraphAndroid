0d7ce0c5bb4f4b895de5b4188e055a66c5cd39bc
Fatih <rizelifatih5353@gmail.com>
2021-02-21 16:22:59 +0300

    added list data fixed bugs

---
+++ README.md
@@ -0,0 +1,27 @@
+# Fatih Kurçenli - GraphExample Application with Kotlin
+
+I learn how to draw a graphic on Android.
+
+# How to Work
+
+![ezgif com-gif-maker (3)](https://user-images.githubusercontent.com/34714108/108626108-8494ee00-745f-11eb-8b62-eba6a0fa6042.gif)
+
+
+
+
+
+# License & Copyright
+
+© Copyright 2021 Fatih Kurçenli 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
--- app/src/main/java/com/autumnsun/graphexample/MainActivity.kt
+++ app/src/main/java/com/autumnsun/graphexample/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.autumnsun.graphexample
 
-import android.graphics.Color.red
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.autumnsun.graphexample.databinding.ActivityMainBinding
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,77 +18,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setLineChartData()
 
-
     }
 
     fun setLineChartData() {
-        var xavier = ArrayList<String>()
-        xavier.add("11.00 AM")
-        xavier.add("12.00 AM")
-        xavier.add("1.00 PM")
-        xavier.add("2.00 PM")
-        xavier.add("3.00 PM")
 
-        val lineentry = ArrayList<Entry>()
-        lineentry.add(Entry(20f, 0.0F))
-        lineentry.add(Entry(30f, 3.0F))
-        lineentry.add(Entry(40f, 2.0F))
-        lineentry.add(Entry(50f, 1.0F))
-        lineentry.add(Entry(60f, 4.0F))
+        val linevalues = ArrayList<Entry>()
+        linevalues.add(Entry(20f, 0.0F))
+        linevalues.add(Entry(30f, 3.0F))
+        linevalues.add(Entry(40f, 2.0F))
+        linevalues.add(Entry(50f, 1.0F))
+        linevalues.add(Entry(60f, 8.0F))
+        linevalues.add(Entry(70f, 10.0F))
+        linevalues.add(Entry(80f, 1.0F))
+        linevalues.add(Entry(90f, 2.0F))
+        linevalues.add(Entry(100f, 5.0F))
+        linevalues.add(Entry(110f, 1.0F))
+        linevalues.add(Entry(120f, 20.0F))
+        linevalues.add(Entry(130f, 40.0F))
+        linevalues.add(Entry(140f, 50.0F))
 
-        val lineyazisi = LineDataSet(lineentry, "First")
-        lineyazisi.color = resources.getColor(R.color.purple_200)
-        val data = LineData(lineyazisi)
+        val linedataset = LineDataSet(linevalues, "First")
+        //We add features to our chart
+        linedataset.color = resources.getColor(R.color.purple_200)
+
+        linedataset.circleRadius = 10f
+        linedataset.setDrawFilled(true)
+        linedataset.valueTextSize = 20F
+        linedataset.fillColor = resources.getColor(R.color.green)
+        linedataset.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
+        //We connect our data to the UI Screen
+        val data = LineData(linedataset)
         binding.getTheGraph.data = data
+        binding.getTheGraph.setBackgroundColor(resources.getColor(R.color.white))
+        binding.getTheGraph.animateXY(2000, 2000,Easing.EaseInCubic)
+
     }
-
-   /* fun setLineChartData(){
-        val entries = ArrayList<Entry>()
-
-//Part2
-        entries.add(Entry(1f, 10f))
-        entries.add(Entry(2f, 2f))
-        entries.add(Entry(3f, 7f))
-        entries.add(Entry(4f, 20f))
-        entries.add(Entry(5f, 16f))
-
-//Part3
-        val vl = LineDataSet(entries, "My Type")
-
-//Part4
-        vl.setDrawValues(false)
-        vl.setDrawFilled(true)
-        vl.lineWidth = 3f
-        vl.fillColor = R.color.purple_200
-        vl.fillAlpha = R.color.purple_700
-
-//Part5
-        binding.getTheGraph.xAxis.labelRotationAngle = 0f
-
-//Part6
-        binding.getTheGraph.data = LineData(vl)
-
-//Part7
-        binding.getTheGraph.axisRight.isEnabled = false
-        //binding.linechart.xAxis.axisMaximum = j+0.1f
-
-//Part8
-        binding.getTheGraph.setTouchEnabled(true)
-        binding.getTheGraph.setPinchZoom(true)
-
-//Part9
-        binding.getTheGraph.description.text = "Days"
-        binding.getTheGraph.setNoDataText("No forex yet!")
-
-//Part10
-        binding.getTheGraph.animateX(1800, Easing.EaseInExpo)
-
-//Part11
-        /* val markerView = CustomMarker(this@ShowForexActivity, R.layout.marker_view)
-         lineChart.marker = markerView */
-    } */
-
-   
-
 
 }
